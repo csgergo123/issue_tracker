9f79711960213a17eb21094953be71eb08b12c86
Csipkes Gergo <csipkesgeri@gmail.com>
2020-05-17 23:03:46 +0200

    Add IssueDao class, represents the DAO for the Issue class

--- src/main/java/issue/IssueDao.java
+++ src/main/java/issue/IssueDao.java
@@ -4,11 +4,47 @@ import com.google.inject.persist.Transactional;
 import issue.Issue;
 import jpa.GenericJpaDao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * DAO class for the {@link Issue} entity.
+ * Extends of GenericJpaDao class.
+ *
+ * @author Csipkes Gergo
+ * @author csipkesgeri@gmail.com
+ * @version 0.2
+ */
 public class IssueDao extends GenericJpaDao<Issue> {
+    /**
+     * Returns the list of issues where the finished date is empty.
+     *
+     * @return the list of issues where the finished date is empty.
+     */
     @Transactional
     public List<Issue> findUnfinished() {
-        return entityManager.createQuery("SELECT t FROM Issue t WHERE t.dateFinished = false ORDER BY t.dateCreated", Issue.class).getResultList();
+        return entityManager.createQuery("SELECT t FROM Issue t WHERE t.dateFinished IS NULL ORDER BY t.dateCreated", Issue.class).getResultList();
+    }
+
+    /**
+     * Optionally returns an issue with the given title and details.
+     *
+     * @param title
+     * @param details
+     * @return Issue (optional) returns an issue with the given title and details.
+     */
+    @Transactional
+    public Optional<Issue> findByTitleAndDetails(String title, String details) {
+        try {
+            return Optional.of(entityManager.createQuery("SELECT i FROM Issue i WHERE i.title  :title AND i.details = :details ORDER BY i.dateCreated", Issue.class)
+                .setParameter("title", title)
+                .setParameter("details", details)
+                .getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 }
