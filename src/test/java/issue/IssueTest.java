f37f2038b6fca0d20805e466559ea5d4340250cc
Csipkes Gergo <csipkesgeri@gmail.com>
2020-05-18 20:28:07 +0200

    Tests for the Issue class

---
+++ src/test/java/issue/IssueTest.java
@@ -0,0 +1,15 @@
+package issue;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IssueTest {
+
+    /*
+    @Test
+    void testIssueArrayValidArgument() {
+        assertThrows(IllegalArgumentException.class, () -> new Issue());
+        assertThrows(IllegalArgumentException.class, () -> new Issue(1, "test title", "test details"));
+    }
+    */
+}
