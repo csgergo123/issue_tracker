bd8810e1004fbd9a7032a0bc74162035a608518b
Csipkes Gergo <csipkesgeri@gmail.com>
2020-05-18 20:29:56 +0200

    Add new tests for the Issue class

--- src/test/java/issue/IssueTest.java
+++ src/test/java/issue/IssueTest.java
@@ -1,15 +1,21 @@
 package issue;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IssueTest {
+    Issue issue = new Issue(1, "Test issue", "Test detail");
 
-    /*
     @Test
-    void testIssueArrayValidArgument() {
-        assertThrows(IllegalArgumentException.class, () -> new Issue());
-        assertThrows(IllegalArgumentException.class, () -> new Issue(1, "test title", "test details"));
+    void testIsSolved() {
+        assertFalse(new Issue().isSolved());
+        assertFalse(issue.isSolved(), "Check solved status");
     }
-    */
+
+    @Test
+    void testSetSolved() {
+        issue.setSolved();
+        assertTrue(issue.isSolved(), "Check solved status");
+    }
 }
